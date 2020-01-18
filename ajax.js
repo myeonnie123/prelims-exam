b6fad665ee80ad5a9ab48d92b0aa789d9c0f3af5
myeonnie123 <57819227+myeonnie123@users.noreply.github.com>
2020-01-18 12:26:29 +0800

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#btn-table").click(function(){
+        $("#container").load("table.html");
+    });
+});
+
+$(document).ready(function(){
+    $("#btn-dialog").click(function(){
+        $("#container").load("dialog.html");
+    });
+});
